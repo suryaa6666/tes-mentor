9c587d4a60a20a3a8534ba3ccdaa34e97ec84317
suryaa6666 <suryaelidanto@gmail.com>
2022-10-10 17:41:53 +0700

    js : loop, and first init also

---
+++ javascript/loop.js
@@ -0,0 +1,46 @@
+// @ts-check
+
+// for loop
+// for (let x = 0; x < 10; x++) {
+//   let y = x + 1;
+//   console.log("ini adalah urutan ke-", y);
+// }
+
+// for-in (picking index / key of array / object)
+
+// const nomor = [1, 2, 3, 4, 5];
+// let hasil = "";
+// for (let x in nomor) {
+//   hasil += nomor[x];
+// }
+
+// console.log(hasil);
+
+// for-of (picking value of array / object)
+
+// const nomor2 = [1, 2, 3, 4, 5];
+// let hasil2 = "";
+// for (let x of nomor2) {
+//   hasil2 += x;
+// }
+
+// console.log(hasil2);
+
+// while loop (when you don't know surely how long the loop is)
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log("angka while", i);
+//   i++;
+// }
+
+// do-while (loop 1 more in the end of while)
+// let i = 1;
+// do {
+//   console.log("do", i);
+//   i++;
+// } while (i < 10);
+// {
+//   console.log("while", i);
+// }
