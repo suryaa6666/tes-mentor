f3e1af321b127aa25c00c42d787741b6ca1a3a37
suryaa6666 <suryaelidanto@gmail.com>
2022-10-10 19:53:28 +0700

    function

--- javascript/loop.js
+++ javascript/loop.js
@@ -1,12 +1,12 @@
 // @ts-check
 
-// for loop
+// for loop --> hint : make loop 0 - 10
 // for (let x = 0; x < 10; x++) {
 //   let y = x + 1;
 //   console.log("ini adalah urutan ke-", y);
 // }
 
-// for-in (picking index / key of array / object)
+// for-in (picking index / key of array / object) --> hint : make array of number, for in
 
 // const nomor = [1, 2, 3, 4, 5];
 // let hasil = "";
@@ -16,7 +16,7 @@
 
 // console.log(hasil);
 
-// for-of (picking value of array / object)
+// for-of (picking value of array / object) --> hint : make same as for in
 
 // const nomor2 = [1, 2, 3, 4, 5];
 // let hasil2 = "";
@@ -26,7 +26,7 @@
 
 // console.log(hasil2);
 
-// while loop (when you don't know surely how long the loop is)
+// while loop (when you don't know surely how long the loop is) --> hint : make variable init first
 
 // let i = 0;
 
@@ -35,7 +35,7 @@
 //   i++;
 // }
 
-// do-while (loop 1 more in the end of while)
+// do-while (loop 1 more in the end of while) --> hint : make variable init, do and add, while
 // let i = 1;
 // do {
 //   console.log("do", i);
